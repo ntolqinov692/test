a02e2575bf105d6d0dad4d291e179be6f894019e
Tolqunov Nurmuhammad <ntolqinov692@gmail.com>
2024-12-02 11:46:05 +0500

    aasa

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        integers.add(4);
+        integers.add(54);
+
+        int index = integers.indexOf(54);
+//        System.out.println(integers.toString());
+
+        LinkedList<Integer> numberlist = new LinkedList<>();
+        numberlist.addLast(10);
+        numberlist.addLast(20);
+        numberlist.addLast(30);
+        numberlist.addLast(40);
+
+        System.out.println(numberlist);
+
+        numberlist.addFirst(5);
+        System.out.println(numberlist);
+        numberlist.removeLast();
+        System.out.println(numberlist.contains(20));
+
+
+
+    }
+}
